8073e77a3967a26a0fb712b6135b1a2245d8335f
jam1garner <8260240+jam1garner@users.noreply.github.com>
2020-10-25 15:48:54 -0400

    Cleanup and Fix typos

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,36 @@
-use color_eyre::eyre;
-
-use battery::Manager;
-use battery::units::ratio::percent;
-
-use notify_rust::Notification;
-
 use cronjob::CronJob;
+use battery::Manager;
+use color_eyre::eyre;
+use notify_rust::Notification;
+use battery::units::ratio::percent;
 
 fn main() -> eyre::Result<()> {
     color_eyre::install()?;
 
-    let mut cron = CronJob::new("Battery Notificatoin", check_battery_job);
+    let mut cron = CronJob::new("Battery Notification", check_battery);
 
+    // Once a minute
     cron.seconds("0");
-
-    cron.start_job();
-
-    Ok(())
+    Ok(cron.start_job())
 }
 
-fn check_battery_job(_: &str) {
-    check_battery().unwrap();
-}
+fn check_battery(_: &str) {
+    (|| -> eyre::Result<()> {
+        let primary_battery = Manager::new()?
+            .batteries()?
+            .next()
+            .ok_or_else(|| eyre::eyre!("No battery found"))??;
 
-fn check_battery() -> eyre::Result<()> {
-    let primary_battery = Manager::new()?
-        .batteries()?
-        .nth(0)
-        .ok_or_else(|| eyre::eyre!("No battery found"))??;
+        let primary_percent = primary_battery.state_of_charge().get::<percent>();
 
-    let primary_percent = primary_battery.state_of_charge().get::<percent>();
-
-    if primary_percent < 10.0 && primary_battery.state() != battery::State::Charging {
-        Notification::new()
-            .summary("Low battery")
-            .icon("low-battery")
-            .body("The primary battery has dropped below 10%")
-            .show()?;
-    }
-    
-    Ok(())
+        if primary_percent < 10.0 && primary_battery.state() != battery::State::Charging {
+            Notification::new()
+                .summary("Low battery")
+                .icon("low-battery")
+                .body("The primary battery has dropped below 10%")
+                .show()?;
+        }
+        
+        Ok(())
+    })().unwrap()
 }
